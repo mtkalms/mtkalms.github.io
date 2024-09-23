4e74cae078489590f359a13f1e7bc3e997cb58ca
Matthias Kalms <mtkalms@gmail.com>
2024-09-22 20:27:55 -0700

    Add social links

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,38 @@ import profile from "./assets/profile.jpg";
 import "./App.css";
 import Avatar from "./components/Avatar";
 import SkillBadge from "./components/SkillBadge";
+import { FaGithub as Github, FaLinkedin as Linkedin, FaEnvelope as Mail } from "react-icons/fa";
 
 function App() {
   return (
-    <div className="flex flex-col gap-5">
-      <Avatar image={profile} className="w-80 ring-white ring-8 shadow-xl" />
+    <div className="flex flex-col gap-5 items-center">
+      <Avatar image={profile} className="w-80 ring-white ring-8 shadow-2xl" />
       <div>
         <h1>Hi. I'm Matt.</h1>
-        <h2 className="">I make software.</h2>
+        <h2 className="text-2xl font-semibold">I make software.</h2>
       </div>
-      <div className="flex justify-around">
-        <SkillBadge skill="python" experience={7} />
+      <div className="flex flex-row justify-around gap-5">
+        <SkillBadge skill="python" experience={8} />
+        <SkillBadge skill="java" experience={5} />
         <SkillBadge skill="typescript" experience={2} />
         <SkillBadge skill="ruby" experience={1} />
+      </div>
+      <div className="flex flex-row justify-around gap-5 items-center">
+        <a href="https://github.com/mtkalms" target="_blank">
+          <Github
+            size={30}
+            className="hover:fill-pink-200 fill-pink-300 hover:animate-pulse"
+          />
+        </a>
+        <a href="https://www.linkedin.com/in/matthias-kalms/" target="_blank">
+          <Linkedin size={32} 
+            className="hover:fill-pink-200 fill-pink-300 hover:animate-pulse"/>
+        </a>
+
+        <a href="mailto:mtkalms@gmail.com" target="_blank">
+          <Mail size={34} 
+            className="hover:fill-pink-200 fill-pink-300 hover:animate-pulse"/>
+        </a>
       </div>
     </div>
   );
--- src/components/SkillBadge.tsx
+++ src/components/SkillBadge.tsx
@@ -1,10 +1,11 @@
 import Badge from "./Badge";
 import { FaPython as Python } from "react-icons/fa";
 import { AiOutlineRuby as Ruby } from "react-icons/ai";
-import { SiTypescript as Typescript } from "react-icons/si"
-import styles from "./SkillBadge.module.css"
+import { SiTypescript as Typescript } from "react-icons/si";
+import { FaJava as Java } from "react-icons/fa";
+import styles from "./SkillBadge.module.css";
 
-type Skill = "python" | "ruby" | "typescript" | "javascript" | "cpp";
+type Skill = "python" | "ruby" | "typescript" | "javascript" | "cpp" | "java";
 
 interface IconProps {
   skill: Skill;
@@ -13,13 +14,13 @@ interface IconProps {
 function Icon({ skill }: IconProps) {
   switch (skill) {
     case "python":
-      return <Python className="w-full h-full"/>;
+      return <Python />;
     case "ruby":
-      return (
-        <Ruby size={36} className="rotate-45 -translate-y-1" />
-      );
+      return <Ruby className="rotate-45 -translate-y-1" />;
     case "typescript":
-      return <Typescript size={36} />
+      return <Typescript />;
+    case "java":
+      return <Java />;
   }
 }
 
@@ -33,9 +34,12 @@ function SkillBadge({ skill, experience, className }: SkillBadgeProps) {
   return (
     <div className={[styles.skill, className].join(" ")}>
       <div className={styles.icon}>
-        <Icon skill={skill}/>
+        <Icon skill={skill} />
       </div>
-      <Badge position="top-right" className={styles.badge}>{`${experience}+`}</Badge>
+      <Badge
+        position="top-right"
+        className={styles.badge}
+      >{`${experience}+`}</Badge>
     </div>
   );
 }
