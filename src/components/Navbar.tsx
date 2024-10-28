7ea8207ec52cc58c28b268a0a63975e680ee36b3
Matthias Kalms <mtkalms@gmail.com>
2024-10-27 19:18:20 -0700

    Add link to portfolio

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,8 +3,12 @@ import ThemeModeToggle from "./ThemeModeToggle";
 function Navbar() {
   return <nav className="absolute w-full top-0 left-0">
     <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-      <div>
-
+      <div className="flex flex-row text-xl">
+        <span className="hover:underline decoration-2 underline-offset-4">
+          <a href="https://mtkalms.github.io/portfolio/">
+            portfolio
+          </a>
+        </span>
       </div>
       <div>
         <ThemeModeToggle/>
