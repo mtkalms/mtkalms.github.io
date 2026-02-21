eb375c7804f4644254aeb9db987120c1e032a555
Matthias Kalms <mtkalms@gmail.com>
2026-02-21 12:49:16 -0800

    Blur navigation bar background

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,20 +1,20 @@
 import ThemeModeToggle from "./ThemeModeToggle";
 
 function Navbar() {
-  return <nav className="fixed w-full top-0 left-0 z-50">
-    <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-      <div className="flex flex-row text-xl">
-        <span className="hover:underline decoration-2 underline-offset-4">
-          <a href="https://mtkalms.github.io/portfolio/">
-            portfolio
-          </a>
-        </span>
+  return (
+    <nav className="fixed w-full top-0 left-0 z-50 backdrop-blur-xl border-b border-b-white/10">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <div className="flex flex-row text-xl">
+          <span className="hover:underline decoration-2 underline-offset-4">
+            <a href="https://mtkalms.github.io/portfolio/">portfolio</a>
+          </span>
+        </div>
+        <div>
+          <ThemeModeToggle />
+        </div>
       </div>
-      <div>
-        <ThemeModeToggle/>
-      </div>
-    </div>
-  </nav>
+    </nav>
+  );
 }
 
 export default Navbar;
