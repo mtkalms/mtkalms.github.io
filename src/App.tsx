0da579c94253bc79e0ecfc1c8868896a72e921aa
Matthias Kalms <mtkalms@gmail.com>
2026-02-22 08:56:34 -0800

    Extract panorama section

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,10 @@ import profile from "./assets/profile.jpg";
 import "./App.css";
 import Avatar from "./components/Avatar";
 import Navbar from "./components/Navbar";
-import Panorama from "./components/Panorama";
 import ParallaxBranches from "./components/ParallaxBranches";
 import Branch1 from "./assets/branch-1.svg?react";
 import Branch2 from "./assets/branch-2.svg?react";
-import Panorama5 from "@/assets/panorama-5.svg?react";
-import Panorama4 from "@/assets/panorama-4.svg?react";
-import Panorama3 from "@/assets/panorama-3.svg?react";
-import Panorama2 from "@/assets/panorama-2.svg?react";
-import Panorama1 from "@/assets/panorama-1.svg?react";
-import Panorama0 from "@/assets/panorama-0.svg?react";
+import PanoramaSection from "./sections/PanoramaSection";
 
 const branchLayers = [
   { Component: Branch1, speed: -0.15 },
@@ -21,27 +15,8 @@ const branchLayers = [
 function App() {
   return (
     <>
-      <Panorama className="bg-(--horizon) fixed top-0 left-0 w-full h-full -z-1">
-        <Panorama.Layer speed={0.1}>
-          <Panorama5 className="fill-(--forest-1)" />
-        </Panorama.Layer>
-        <Panorama.Layer speed={0.2}>
-          <Panorama4 className="fill-(--forest-2)" />
-        </Panorama.Layer>
-        <Panorama.Layer speed={0.35}>
-          <Panorama3 className="fill-(--forest-3)" />
-        </Panorama.Layer>
-        <Panorama.Layer speed={0.5}>
-          <Panorama2 className="fill-(--forest-4)" />
-        </Panorama.Layer>
-        <Panorama.Layer speed={0.7}>
-          <Panorama1 className="fill-(--forest-5)" />
-        </Panorama.Layer>
-        <Panorama.Layer speed={0.9}>
-          <Panorama0 className="fill-(--forest-6)" />
-        </Panorama.Layer>
-      </Panorama>
       <Navbar />
+      <PanoramaSection />
       <div className="main">
         <div className="flex flex-col gap-8 items-center">
           <Avatar
