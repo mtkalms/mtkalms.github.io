3cc50ccd812092a2ee6036a4531e8bab0282f1b8
Matthias Kalms <mtkalms@gmail.com>
2026-02-21 16:08:55 -0800

    Add parallax section

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,14 @@ import "./App.css";
 import Avatar from "./components/Avatar";
 import Navbar from "./components/Navbar";
 import Panorama from "./components/Panorama";
+import ParallaxBranches from "./components/ParallaxBranches";
+import Branch1 from "./assets/branch-1.svg?react";
+import Branch2 from "./assets/branch-2.svg?react";
+
+const branchLayers = [
+  { Component: Branch1, speed: -0.15 },
+  { Component: Branch2, speed: -0.25 },
+];
 
 function App() {
   useEffect(() => {
@@ -36,6 +44,21 @@ function App() {
             </h1>
           </div>
         </div>
+        <ParallaxBranches layers={branchLayers}>
+          <h2 className="glow-heading">Textual Light and Magic</h2>
+          <div className="text-column">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod 
+              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+              quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            </p>
+            <p>
+              Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore 
+              eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt 
+              in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+          </div>
+        </ParallaxBranches>
       </div>
     </>
   );
