f71a248dcc4036c92595103b409be2a46059a71e
Matthias Kalms <mtkalms@gmail.com>
2026-02-23 10:46:10 -0800

    More cleanup on textual section

--- src/sections/Textual.tsx
+++ src/sections/Textual.tsx
@@ -2,29 +2,47 @@ import Parallax from "../components/Parallax";
 
 function Textual() {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center text-center">
+    <div className="min-h-screen flex flex-col items-center justify-center">
       <div>
-        <Parallax className="relative overflow-visible">
-          <Parallax.Layer speed={0.05} className="relative t-12">
-            <h2 className="glow-heading leading-none">
-              Textual Light <br />
-              and Magic
-            </h2>
-          </Parallax.Layer>
-        </Parallax>
-        <div className="text-column">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat.
-          </p>
-          <p>
-            Duis aute irure dolor in reprehenderit in voluptate velit esse
-            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-            cupidatat non proident, sunt in culpa qui officia deserunt mollit
-            anim id est laborum.
-          </p>
+        <Parallax.Layer speed={0.05} className="relative my-12">
+          <h2 className="glow leading-none uppercase md:text-7xl text-5xl font-bold">
+            Textual Light <br />
+            and Magic
+          </h2>
+        </Parallax.Layer>
+        <div className="flex flex-col lg:flex-row items-center justify-between gap-8">
+          <Parallax className="relative overflow-visible">
+            <Parallax.Layer
+              speed={0.05}
+              className="relative mb-12 w-full text-left"
+            >
+              <pre className="glow leading-none font-mono text-lg lg:text-2xl">
+                {/* prettier-ignore */}
+                <>
+              <span>┬┬╮    ╷       .   ╷  ┐    </span><br/>
+              <span>│││╭╮╭┐┼ ┌╮┬╮╭┐┐ ╭┐┼┐╷│╭╮╭┐</span><br/>
+              <span>│││││╰╮│ ╭┤││╰╮│ ╰╮││││├┘╰╮</span><br/>
+              <span>╵╵╵╰╯└╯╰ ╰┘╵╵└╯╵ └╯╰╰┤╰╰╴└╯</span><br/>
+              <span>                    └╯     </span><br/>
+              </>
+                enter <span className="animate-pulse">█</span>
+              </pre>
+            </Parallax.Layer>
+          </Parallax>
+          <div className="text-left max-w-xl lg:p-12 text-(--forest-1)">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </p>
+            <p>
+              Duis aute irure dolor in reprehenderit in voluptate velit esse
+              cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+              cupidatat non proident, sunt in culpa qui officia deserunt mollit
+              anim id est laborum.
+            </p>
+          </div>
         </div>
       </div>
     </div>
