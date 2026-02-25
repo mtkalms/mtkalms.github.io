d58650891d49f835eedb9eae5dfbe31817504f8f
Matthias Kalms <mtkalms@gmail.com>
2026-02-25 08:40:06 -0800

    Navbar text back to white

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import ThemeModeToggle from "./ThemeModeToggle";
 
 function Navbar() {
   return (
-    <nav className="fixed top-0 left-0 z-50 w-full border-b border-b-white/10 backdrop-blur-xl">
+    <nav className="fixed top-0 left-0 z-50 w-full border-b border-b-white/10 text-white backdrop-blur-xl">
       <div className="mx-auto flex max-w-screen-xl flex-wrap items-center justify-between px-16 py-4">
         <div className="flex flex-row text-xl">
           <span className="decoration-2 underline-offset-4 hover:underline">
