cdde7ab85fe5f68ad82b40d93d4e7eb15e016030
Matthias Kalms <mtkalms@gmail.com>
2026-02-28 20:49:56 -0800

    Fix pattern color

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
       <div className="main">
         <Textual />
       </div>
-      <div className="h-40 w-full bg-(--text-default) mask-[url('./assets/pattern-stack.svg')] bg-size-[auto_100%] bg-repeat-x opacity-25" />
+      <div className="h-40 w-full bg-(--forest-1) mask-[url('./assets/pattern-stack.svg')] bg-size-[auto_100%] bg-repeat-x" />
       <footer className="bottom-0 w-full py-12">
         <div className="mx-auto flex max-w-screen-xl items-center justify-between px-16 py-4">
           <p className="text-sm opacity-80">
