952f6c496810a7519f5531766fe2320aa9478022
Matthias Kalms <mtkalms@gmail.com>
2026-02-28 23:54:19 -0800

    More glassmorphism in navigation

--- src/App.tsx
+++ src/App.tsx
@@ -14,22 +14,25 @@ const navbarItems = [
 function App() {
   return (
     <>
-      <nav className="fixed top-0 left-0 z-50 w-full border-b border-b-white/10 text-white backdrop-blur-xl">
-        <div className="mx-auto flex max-w-screen-xl flex-wrap items-center justify-between px-16">
-          <div className="flex flex-row">
-            {navbarItems.map((item) => (
-              <div
-                className="transition-border border-b border-b-white/0 px-2 py-3 text-xl duration-300 hover:border-b-white"
-                key={item.label}
-              >
-                <a href={item.href}>{item.label}</a>
-              </div>
-            ))}
-          </div>
-          <div className="flex flex-row">
-            <ThemeModeToggle />
+      <nav className="fixed top-0 left-0 z-50 w-full border-b border-b-white/10 text-white">
+        <div className="-mb-[2px] backdrop-blur-xl">
+          <div className="mx-auto flex max-w-screen-xl flex-wrap items-center justify-between px-16">
+            <div className="flex flex-row">
+              {navbarItems.map((item) => (
+                <div
+                  className="transition-border border-b-2 border-b-transparent px-2 py-3 text-xl duration-300 hover:border-b-white"
+                  key={item.label}
+                >
+                  <a href={item.href}>{item.label}</a>
+                </div>
+              ))}
+            </div>
+            <div className="flex flex-row">
+              <ThemeModeToggle />
+            </div>
           </div>
         </div>
+        <div className="h-[2px] w-full backdrop-blur-3xl" />
       </nav>
       <Panorama />
       <div className="main">
