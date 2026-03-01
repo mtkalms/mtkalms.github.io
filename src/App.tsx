22b00f1953e3ddebea90a87227c93bb6a038c43f
Matthias Kalms <mtkalms@gmail.com>
2026-02-28 17:15:33 -0800

    Add lynchian pattern divider

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,6 @@ import Navbar from "./components/Navbar";
 import Panorama from "./sections/Panorama";
 import Textual from "./sections/Textual";
 import SocialBadge from "./components/SocialBadge";
-import Introduction from "./sections/Introduction";
-
 function App() {
   return (
     <>
@@ -13,8 +11,9 @@ function App() {
       <div className="main">
         <Textual />
       </div>
+      <div className="h-40 w-full bg-(--text-default) mask-[url('./assets/pattern-stack.svg')] bg-size-[auto_100%] bg-repeat-x opacity-25" />
       <footer className="bottom-0 w-full py-12">
-        <div className="flex items-center justify-between px-16 py-4">
+        <div className="mx-auto flex max-w-screen-xl items-center justify-between px-16 py-4">
           <p className="text-sm opacity-80">
             © {new Date().getFullYear()} mtkalms. All rights reserved.
           </p>
